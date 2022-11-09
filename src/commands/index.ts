d289fe7b1a88b21bbfd8e37bb7c9413fb323dbd1
raosan <raosanf.lillahi@gmail.com>
2022-11-09 13:34:39 +0700

    eslintrc

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,23 +1,34 @@
-import {Command, Flags} from '@oclif/core'
+import { Command, Flags } from "@oclif/core";
 
 export default class PrismaSeeder extends Command {
-  static description = 'Check if migration success by providing current and new path_to_schema_prisma_file'
+  static description =
+    "Check if migration success by providing current and new path_to_schema_prisma_file";
 
   static examples = [
     `$ oex --new <path_to_new_schema_prisma_file> --current <path_to_current_schema_prisma_file>
 Start checking...
 Finish checking.
 `,
-  ]
+  ];
 
   static flags = {
-    new: Flags.string({char: 'n', description: 'path to new schema prisma file', required: true}),
-    current: Flags.string({char: 'c', description: 'path to current schema prisma file', required: true}),
-  }
+    new: Flags.string({
+      char: "n",
+      description: "path to new schema prisma file",
+      required: true,
+    }),
+    current: Flags.string({
+      char: "c",
+      description: "path to current schema prisma file",
+      required: true,
+    }),
+  };
 
   async run(): Promise<void> {
-    const {flags} = await this.parse(PrismaSeeder)
+    const { flags } = await this.parse(PrismaSeeder);
 
-    this.log(`The new schema path: ${flags.new} \nand the current schema path: ${flags.current}`)
+    this.log(
+      `The new schema path: ${flags.new} \nand the current schema path: ${flags.current}`
+    );
   }
 }
